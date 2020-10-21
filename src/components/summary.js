6158a62366078c158058fcd45955f940a900d04a
ram <pewlitsun@naver.com>
2020-10-22 01:32:09 +0900

    create click event

--- src/components/summary.js
+++ src/components/summary.js
@@ -1,10 +1,50 @@
-import React from "react"
+import React, { useEffect, useState } from "react"
 import '../css/summary.css'
 
-const Summary = ({contents}) => {
+const Summary = ({summaryObj}) => {
+    const [showBtn,setShowBtn] = useState(false);
+    const [isEditable,setIsEditable] = useState(false);
+    let thisSmr;
+    const onSummaryClick = (event) =>{
+        if(!showBtn){
+            thisSmr.querySelector('.smr_buttonSet').style.display='';
+        }else{
+            thisSmr.querySelector('.smr_buttonSet').style.display='none';
+        }
+        setShowBtn((prev) => !prev)
+    }
+
+    const onAddButtonClick = (event) =>{
+        event.stopPropagation();
+    }
+    
+    const onEditButtonClick = (event) =>{
+        event.stopPropagation();
+
+        thisSmr.querySelector('.smr_editInput').style.display='';
+        thisSmr.querySelector('.smr_editInput').value=
+        thisSmr.querySelector('.smr_summary').innerText;
+        // thisSmr.querySelector('.smr_summary').innerText=''
+
+    }
+
+    useEffect(()=>{
+        thisSmr = document.getElementById(summaryObj.summaryId)
+    })
+
     return(
-        <div className ='smr_div'>
-            <h5 className='smr_summary font_sans'>{contents}</h5>
+        <div id={summaryObj.summaryId} className ='smr_summarySet' onClick={onSummaryClick}>
+            <span className='smr_buttonSet'>
+                <span className='smr_addSummaryButton'onClick={onAddButtonClick}></span>
+                <span className='smr_editButton'onClick={onEditButtonClick}></span>
+            </span>
+            {/* <span className='smr_summarySet'> */}
+            <h5 className='smr_summary font_sans'>
+                {summaryObj.contents}
+                <input className='smr_editInput font_sans'/>
+            </h5>
+            {/* </span> */}
+
         </div>
     )
 }
--- src/components/summarySet.js
+++ src/components/summarySet.js
@@ -1,25 +1,37 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Summary from './summary';
 
 function SummarySet(){
-    const test = (event) =>{
+
+
+    const test = (event) => {
         const {
-            target:{checked}
+            target: { checked }
         } = event;
 
-            document.querySelectorAll('.smr_summary').forEach((v,i)=>{
-                v.classList.toggle('font_sans',!checked);
-                v.classList.toggle('font_serif',checked);
-            })
+        document.querySelectorAll('.smr_summary').forEach((v, i) => {
+            v.classList.toggle('font_sans', !checked);
+            v.classList.toggle('font_serif', checked);
+        })
+        document.querySelectorAll('.smr_editInput').forEach((v, i) => {
+            v.classList.toggle('font_sans', !checked);
+            v.classList.toggle('font_serif', checked);
+        })
     }
-    
+
+    useEffect(()=>{
+        document.querySelectorAll('.smr_editInput').forEach(v=>{v.style.display='none'})
+        document.querySelectorAll('.smr_buttonSet').forEach(v=>{v.style.display='none'})
+
+    })
+
     return(
         <>
         <input id='test' type='checkbox' onClick={test}/>
         <div className="summaryset_div">
-            <Summary key='1' contents='이 사이트는 세줄로 요약하여 글을 작성하는 사이트입니다.'></Summary>
-            <Summary key='2' contents='요약한 각각의 문장은 다시 세줄로 풀어쓸 수 있습니다.'></Summary>
-            <Summary key='3' contents='모든 문장을 세줄로 나타내고나면 글쓰기가 완료됩니다.'></Summary>
+            <Summary summaryObj={{summaryId:'1',contents:'이 사이트는 세줄로 요약하여 글을 작성하는 사이트입니다.'}} key='1' ></Summary>
+            <Summary summaryObj={{summaryId:'2',contents:'요약한 각각의 문장은 다시 세줄로 풀어쓸 수 있습니다.'}} key='2' ></Summary>
+            <Summary summaryObj={{summaryId:'3',contents:'모든 문장을 세줄로 나타내고나면 글쓰기가 완료됩니다.'}} key='3' ></Summary>
         </div>
         </>
     )
